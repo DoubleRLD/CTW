392f4c31be8d67777b37b4b8045e93d1508b19bc
lthomas130 <lincdthomas@gmail.com>
2026-07-06 16:20:53 -0400

    Delete backend/src directory

--- backend/src/api/roommateProfiles.js
+++
@@ -1,9 +0,0 @@
-import { api } from "./client";
-
-export const roommateProfilesApi = {
-  getMine: (semester, semesterYear) =>
-    api.get(`/roommate-profiles/me?semester=${semester}&semesterYear=${semesterYear}`, { auth: true }),
-  upsert: (payload) => api.post("/roommate-profiles", payload, { auth: true }),
-  browse: (semester, semesterYear) =>
-    api.get(`/roommate-profiles/browse?semester=${semester}&semesterYear=${semesterYear}`, { auth: true }),
-};
